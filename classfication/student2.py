ff4291913c857082c543c42410fcecf0a7417bf2
Jamikidu <kangjh1994@gmail.com>
2023-04-24 18:15:44 +0900

    turtle2

---
+++ classfication/student2.py
@@ -0,0 +1,23 @@
+# Student
+# 클래스 생성 함수 def__init__()
+class Student:
+    # 생성자 함수(멤버 함수)
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+
+    def __str__(self):
+        return f'이름 : {self.name}, 학년 : {self.grade}'
+
+    def learn(self):
+        print("수업을 듣습니다.")
+
+# 메인 영역
+if __name__ == "__main__":
+    s1 = Student('김하나', 1)
+    print(s1)
+    s1.learn()
+
+    s2 = Student('홍길동', 3)
+    print(s2)
+    s2.learn()
